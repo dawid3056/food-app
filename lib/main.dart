6b3a8eedbd0c3e4d85525a6b9641bd08da16ca5c
Dawid <dawidkaleta@student.agh.edu.pl>
2024-03-09 12:21:55 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/pages/categories.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final theme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(102, 51, 172, 55),
+    brightness: Brightness.dark,
+  ),
+
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: theme,
+      home: const CategoriesPage(),
+    );
+  }
+}
---
+++ lib/pages/categories.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/data/data.dart';
+import 'package:food_app/widgets/category_grid_item.dart';
+
+class CategoriesPage extends StatelessWidget {
+  const CategoriesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Pick your category',
+        ),
+        centerTitle: true,
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
+        ],
+      ),
+    );
+  }
+}
