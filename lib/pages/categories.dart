3621454ed2bf8b2c2497f4dd6ad1852e50a3a5a5
Dawid <dawidkaleta@student.agh.edu.pl>
2024-03-11 17:28:34 +0100

    added navigation to meals page

--- lib/pages/categories.dart
+++ lib/pages/categories.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:food_app/data/data.dart';
+import 'package:food_app/pages/meals.dart';
 import 'package:food_app/widgets/category_grid_item.dart';
 
 class CategoriesPage extends StatelessWidget {
   const CategoriesPage({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const MealsPage(meals: [], title: 'title'),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +33,10 @@ class CategoriesPage extends StatelessWidget {
         ),
         children: [
           for (final category in availableCategories)
-            CategoryGridItem(category: category),
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () => _selectCategory(context),
+            ),
         ],
       ),
     );
--- lib/pages/meals.dart
+++ lib/pages/meals.dart
@@ -1,10 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/models/meal.dart';
 
 class MealsPage extends StatelessWidget {
-  const MealsPage({super.key});
+  const MealsPage({
+    super.key,
+    required this.meals,
+    required this.title,
+  });
+
+  final String title;
+  final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Widget content = ListView.builder(
+      itemBuilder: (ctx, index) => Text(
+        meals[index].title,
+      ),
+    );
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Nothing is here..',
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Try to select a different category',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => Text(
+          meals[index].title,
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(title),
+      ),
+      body: content,
+    );
   }
 }
